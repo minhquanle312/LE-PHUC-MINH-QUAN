3dfca360bd5119f78181a0beb170853b9f0c4ea1
Minh Quân Lê <minhquanle@gmail.com>
2024-04-17 00:51:49 +0700

    feat(UI): fix responsive

--- src/problem2/src/components/CurrencyTable.tsx
+++ src/problem2/src/components/CurrencyTable.tsx
@@ -8,38 +8,90 @@ export const CurrencyTable = () => {
   const { formattedData, tokenOptions } = useExchangeRate()
 
   return (
-    <div className={classes['table-wrapper']}>
-      <div className={classes['table-scroll']}>
-        <table>
-          <thead>
-            <tr>
-              <th>Cryptocurrency name</th>
-              <th>Price, USD</th>
-              <th>Last updated</th>
-            </tr>
-          </thead>
-          <tbody>
-            {tokenOptions.map((currency) => (
-              <tr key={currency}>
-                <td className={classes['currency-name']}>
+    <>
+      <div className={classes['table-wrapper']}>
+        <div className={classes['table-scroll']}>
+          {tokenOptions.map((currency) => (
+            <div key={currency} className={classes['card']}>
+              <div className={classes['card-header']}>
+                <div className={classes['currency-name']}>
                   <ReactSVG
                     src={`${BASE_URL_SVG}/${
                       MAPPING_EDGE_CASE_ICON?.[currency] || currency
                     }.svg`}
                   />
                   {currency}
-                </td>
-                <td>{formattedData[currency][0].price}</td>
-                <td>
+                </div>
+                <div>
                   {new Date(formattedData[currency][0].date).toLocaleString(
                     'en-US'
                   )}
-                </td>
+                </div>
+              </div>
+              <div>
+                <strong>Price: </strong>
+                {formattedData[currency][0].price}
+              </div>
+            </div>
+          ))}
+
+          <table>
+            <thead>
+              <tr>
+                <th>Cryptocurrency name</th>
+                <th>Price, USD</th>
+                <th>Last updated</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {tokenOptions.map((currency) => (
+                <tr key={currency}>
+                  <td>
+                    <div className={classes['currency-name']}>
+                      <ReactSVG
+                        src={`${BASE_URL_SVG}/${
+                          MAPPING_EDGE_CASE_ICON?.[currency] || currency
+                        }.svg`}
+                      />
+                      {currency}
+                    </div>
+                  </td>
+                  <td>{formattedData[currency][0].price}</td>
+                  <td>
+                    {new Date(formattedData[currency][0].date).toLocaleString(
+                      'en-US'
+                    )}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
+
+      {tokenOptions.map((currency) => (
+        <div key={currency} className={classes['card']}>
+          <div className={classes['card-header']}>
+            <div className={classes['currency-name']}>
+              <ReactSVG
+                src={`${BASE_URL_SVG}/${
+                  MAPPING_EDGE_CASE_ICON?.[currency] || currency
+                }.svg`}
+              />
+              {currency}
+            </div>
+            <div>
+              {new Date(formattedData[currency][0].date).toLocaleString(
+                'en-US'
+              )}
+            </div>
+          </div>
+          <div>
+            <strong>Price: </strong>
+            {formattedData[currency][0].price}
+          </div>
+        </div>
+      ))}
+    </>
   )
 }
